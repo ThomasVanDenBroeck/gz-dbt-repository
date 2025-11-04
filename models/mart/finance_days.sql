e4a6049f5bc979dde6fb7fe2bce1362aecedc711
Thomas VAN DEN BROECK <thomas.vdb.fr@gmail.com>
2025-11-04 14:43:18 +0000

    created a mart with all kpis attended

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,13 @@
+SELECT 
+date_date,
+COUNT(orders_id) AS Nb_transactions,
+ROUND(SUM(evenue),2) AS revenue,
+ROUND(AVG(evenue),2) AS Average_basket,
+ROUND(SUM(operationnal_margin),2) AS operationnal_margin,
+ROUND(SUM(purchase_cost),2) AS purchase_cost,
+ROUND(SUM(shipping_fee),2) AS shipping_fee,
+ROUND(SUM(ship_cost)+SUM(logcost),2) AS operationnal_cost,
+ROUND(SUM(quantity),2) AS Nb_products
+FROM {{ ref('int_orders_operational')}}
+GROUP BY date_date
+ORDER BY date_date DESC
